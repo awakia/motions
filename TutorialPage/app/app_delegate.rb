796fa6b8d2e99f006770a62c2d3e1793c661ebdd
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-16 17:55:03 +0900

    Instantiate view without indicating name

--- TutorialPage/app/app_delegate.rb
+++ TutorialPage/app/app_delegate.rb
@@ -2,7 +2,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @storyboard = UIStoryboard.storyboardWithName('TutorialStoryboard', bundle:nil)
-    nav_controller = UINavigationController.alloc.initWithRootViewController(@storyboard.instantiateViewControllerWithIdentifier('WelcomeViewController'))
+    nav_controller = UINavigationController.alloc.initWithRootViewController(@storyboard.instantiateInitialViewController)
     @window.rootViewController = nav_controller
     @window.makeKeyAndVisible
     true
