1bb10825a64fd78d29f28c5d3a48db9c389e6b5c
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-18 10:54:29 +0900

    Reflect fetched data to table view

--- RestKit/app/coffee_shop_select_controller.rb
+++ RestKit/app/coffee_shop_select_controller.rb
@@ -1,11 +1,24 @@
 class CoffeeShopSelectController < UITableViewController
   def viewDidLoad
     view.dataSource = view.delegate = self
+    @json = {}
     server = Server.instance
-    server.get("/coffee_shops.json")
+    server.get("/coffee_shops.json", method(:request).to_proc)
   end
 
-  def numberOfRowsInSection(section)
-    return 1
+  def tableView(tableView, numberOfRowsInSection:section)
+    p @json.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier("test") || UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:"test")
+    cell.textLabel.text = @json[indexPath.row]['name']
+    cell.detailTextLabel.text = @json[indexPath.row].to_s
+    cell
+  end
+
+  def request(request, result)
+    @json = result.dictionary.values.first
+    tableView.reloadData
   end
 end
