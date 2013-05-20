23bf01396756d53399edb8d3f31fd8c663989003
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-20 14:36:45 +0900

    Add account getter for facebook

--- SocialFramework/app/SocialViewController.rb
+++ SocialFramework/app/SocialViewController.rb
@@ -1,10 +1,39 @@
 class SocialViewController < UIViewController
   extend IB
 
+  def facebookAccount
+    return @facebookAccount if @facebookAccount
+
+    accountStore = ACAccountStore.new
+    facebookAccountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierFacebook)
+    options = {
+      ACFacebookAppIdKey => NSBundle.mainBundle.objectForInfoDictionaryKey('FACEBOOK_APP_ID'),  # app id
+      ACFacebookPermissionsKey => ['email'],  # read privileges
+      ACFacebookAudienceKey => ACFacebookAudienceOnlyMe  # scope of disclosure
+    }
+    accountStore.requestAccessToAccountsWithType(
+      facebookAccountType,
+      options:options, completion:->(granted, error) do
+        if granted
+          accounts = accountStore.accountsWithAccountType:facebookAccountType
+          @facebookAccount = accounts.lastObject
+          @facebookCredential = @facebookAccount.credential
+          @facebookToken = @facebookCredential.oauthToken
+          NSLog('fbAccessToken : %@', facebookToken)
+        else
+          NSLog('error getting permission : %@', error)
+        end
+      end
+    )
+    @facebookAccount
+  end
+
+
   def facebookRequest
     url = 'https://graph.facebook.com/me'.nsurl
     params = { fields: 'id,name' }
     request = SLRequest.requestForServiceType(SLServiceTypeFacebook, requestMethod:SLRequestMethodGET, URL:url, parameters:params)
+    request.setAccount(self.facebookAccount)
     request.performRequestWithHandler(method(:slRequestHandler).to_proc)
   end
 
