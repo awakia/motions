b36463c9ebc32695f3a4d9c5960196353dab01f6
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-20 17:11:24 +0900

    Add error handling

--- SocialFramework/app/SocialViewController.rb
+++ SocialFramework/app/SocialViewController.rb
@@ -21,13 +21,27 @@ class SocialViewController < UIViewController
           @facebookToken = @facebookCredential.oauthToken
           NSLog('fbAccessToken : %@', facebookToken)
         else
-          NSLog('error getting permission : %@', error)
+          # see http://stackoverflow.com/questions/12630066/acaccountstore-error-6-and-8
+          NSLog('error code : %@', error)
+          if error.code == 6
+            NSLog("Please set up Facebook Account!")
+            # # http://stackoverflow.com/questions/14145322/ios-6-social-framework-not-going-to-settings-or-no-alert
+            # # SLComposeViewController.presentViewController show account setting alert by default,
+            # # but there is not the way to call that alert manually for now...
+            # mySLComposerSheet = SLComposeViewController.composeViewControllerForServiceType(SLServiceTypeFacebook)
+            # mySLComposerSheet.setInitialText("")
+            # self.presentViewController(mySLComposerSheet, animated:true, completion:nil)
+
+            # Even if the error "CGImageCreate: invalid image size: 0 x 0" occurs in simulator,
+            # it does not happen in actual device (http://sugumura.hatenablog.com/entry/2013/04/09/151131)
+          else
+            NSLog("Error accessing account: %s", error.localizedDescription)
+          end
         end
       end
     )
     @facebookAccount
   end
-
 
   def facebookRequest
     url = 'https://graph.facebook.com/me'.nsurl
